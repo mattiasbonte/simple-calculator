ec2159e957b65045677c36f25df67201e44592c9
Mattias Bonte <mattias_bonte@hotmail.com>
2020-07-08 15:53:49 +0200

    Calculation part done

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,42 @@
-// Simple Calculator
+/* ----------------- */
+/* Simple Calculator */
+/* ----------------- */
 
-// Get input and store inside variables that will not change
-const input1 = document.querySelector('#input1');
-const select = document.querySelector('#select');
-const input2 = document.querySelector('#input2');
-const output = document.querySelector('#output');
+// Get input elements and store them inside variables that will not change
+const input1 = document.querySelector('.js-input1');
+const select = document.querySelector('.js-select');
+const input2 = document.querySelector('.js-input2');
+const output = document.querySelector('.js-output');
+const equal = document.querySelector('.js-equal');
 
 // Calculator function
 function calculate() {
   let inp1 = Number(input1.value);
   let sel = select.value;
   let inp2 = Number(input2.value);
+  let out;
+
+  if (sel === '+') {
+    out = inp1 + inp2;
+    output.value = out.toString();
+  } else if (sel === '-') {
+    out = inp1 - inp2;
+    output.value = out.toString();
+  } else if (sel === '*') {
+    out = inp1 * inp2;
+    output.value = out.toString();
+  } else if (sel === '/') {
+    out = (inp1 / inp2).toFixed(2);
+    output.value = out.toString();
+  } else {
+    console.log('you broke it');
+  }
 }
+
+// Event Listeners
+equal.addEventListener('click', calculate);
+input1.addEventListener('change', calculate);
+input1.addEventListener('keyup', calculate);
+select.addEventListener('change', calculate);
+input2.addEventListener('change', calculate);
+input2.addEventListener('keyup', calculate);

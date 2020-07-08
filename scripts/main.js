c7b4a278ac0285a80da17736cdbe8707448aa652
Mattias Bonte <mattias_bonte@hotmail.com>
2020-07-08 14:18:54 +0200

    JS init

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
 // Simple Calculator
+
+// Get input and store inside variables that will not change
+const input1 = document.querySelector('#input1');
+const select = document.querySelector('#select');
+const input2 = document.querySelector('#input2');
+const output = document.querySelector('#output');
+
+// Calculator function
+function calculate() {
+  let inp1 = Number(input1.value);
+  let sel = select.value;
+  let inp2 = Number(input2.value);
+}
